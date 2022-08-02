5a81a49ada4dafa0873e1a04fcd0271e12f3bf61
benNjogu <kamandemachafi@gmail.com>
2022-08-02 14:50:34 +0300

    Creating a JFrame basics part 1

--- src/com/keytech/Main.java
+++ src/com/keytech/Main.java
@@ -1,8 +1,25 @@
 package com.keytech;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("let the fun begin");
+        //A JFrame is a GUI window to add components to
+        JFrame frame = new JFrame();//creates a frame
+        frame.setTitle("Mens Conference");//sets title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application
+        frame.setResizable(false);//prevent frame from being resized
+
+        frame.setSize(420, 420);//sets the x-dimension and the y-dimension of the frame
+        frame.setVisible(true);//make frame visible
+
+        //below we use getResource since our image is located in a package
+        ImageIcon image = new ImageIcon(Main.class.getResource("/images/TDD.PNG"));//create image icon
+        frame.setIconImage(image.getImage());//change icon of frame
+        frame.getContentPane().setBackground(Color.green);//will change colour of background
+        frame.getContentPane().setBackground(new Color(0x1371999));//will change colour of background
+
     }
 }
