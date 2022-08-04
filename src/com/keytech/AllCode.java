466086d14e54c0a3b31ca723308c93ebbaee55fa
benNjogu <kamandemachafi@gmail.com>
2022-08-04 15:29:50 +0300

    JButton implementing the actionListener interface

--- src/com/keytech/AllCode.java
+++ src/com/keytech/AllCode.java
@@ -1,6 +1,9 @@
 package com.keytech;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 public class AllCode {
     /*
     CHAPTER ONE and TWO
@@ -38,5 +41,42 @@ public class AllCode {
         frame.pack();//resize the frame to accommodate all the components that you have
 
         CHAPTER 3.
-*/
+         //JPanel - a GUI components that functions as a container to hold other components
+
+        ImageIcon image = new ImageIcon(Main.class.getResource("/images/one.PNG"));
+
+        JLabel label = new JLabel();
+        label.setText("Dah!");
+        label.setIcon(image);
+        /**@the below line only apply when using border layouts
+         *
+    //label.setVerticalAlignment(JLabel.BOTTOM);
+    //label.setHorizontalAlignment(JLabel.RIGHT);
+       /* label.setBounds(100, 100, 75, 75);//if not using boarder layout
+
+    JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+        redPanel.setLayout(null);
+
+    JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(null);
+
+    JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, redPanel.getHeight(), 500, 250);
+        greenPanel.setLayout(null);
+
+    JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(750, 750);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        greenPanel.add(label);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);*/
+
 }
--- src/com/keytech/Main.java
+++ src/com/keytech/Main.java
@@ -7,42 +7,9 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        //JPanel - a GUI components that functions as a container to hold other components
+        //JButton - a button that performs an action when clicked on
 
-        ImageIcon image = new ImageIcon(Main.class.getResource("/images/one.PNG"));
+        new MyFrame();
 
-        JLabel label = new JLabel();
-        label.setText("Dah!");
-        label.setIcon(image);
-        /**@the below line only apply when using border layouts
-         * */
-        //label.setVerticalAlignment(JLabel.BOTTOM);
-        //label.setHorizontalAlignment(JLabel.RIGHT);
-        label.setBounds(100, 100, 75, 75);//if not using boarder layout
-
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 250, 250);
-        redPanel.setLayout(null);
-
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250, 0, 250, 250);
-        bluePanel.setLayout(null);
-
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, redPanel.getHeight(), 500, 250);
-        greenPanel.setLayout(null);
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750, 750);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        greenPanel.add(label);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(greenPanel);
     }
 }
