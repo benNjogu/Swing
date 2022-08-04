9c76d587be043b33cdbdbeebaf74b11ce0dadde4
benNjogu <kamandemachafi@gmail.com>
2022-08-04 14:53:05 +0300

    JPanel with boarderLayouts

---
+++ src/com/keytech/AllCode.java
@@ -0,0 +1,42 @@
+package com.keytech;
+
+
+public class AllCode {
+    /*
+    CHAPTER ONE and TWO
+
+    //JLabel is a GUI display area for a string of text, an image or both
+
+    ImageIcon image = new ImageIcon(Main.class.getResource("/images/bro1.jpg"));
+    Border border = BorderFactory.createLineBorder(new Color(13, 7, 99), 3);
+
+    JLabel label = new JLabel();//Create a label
+        label.setText("Dude, do you write code?");//set text of Label.
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);//set text LEFT, CENTER or RIGHT of imageIcon
+        label.setVerticalTextPosition(JLabel.TOP);//set text TOP, CENTER or BOTTOM of imageIcon
+        label.setForeground(new Color(13, 7, 99));//set font color of text
+        label.setFont(new Font("MV Boli", Font.ROMAN_BASELINE, 48));//set font of text
+        label.setIconTextGap(-4);//set gap of text to image
+        label.setBackground(Color.darkGray);//set background color
+        label.setOpaque(true);//display background color
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);//set vertical position of icon+text within label
+        label.setHorizontalAlignment(JLabel.CENTER);//set horizontal position of icon+text within label
+    //label.setBounds(100,100,240, 240);//set x,y position within frame as well as dimensions
+
+
+    JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+    //frame.setLayout(null);
+        frame.setVisible(true);
+        frame.add(label);
+        frame.setBackground(new Color(0x1371999));
+        frame.setIconImage(image.getImage());
+    //NB: Add all your components before using pack
+        frame.pack();//resize the frame to accommodate all the components that you have
+
+        CHAPTER 3.
+*/
+}
