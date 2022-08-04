35e1bcc83e2773ea364bdf1c9e71928d59e8728f
benNjogu <kamandemachafi@gmail.com>
2022-08-04 15:07:21 +0300

    Basics of JPanels

--- src/com/keytech/Main.java
+++ src/com/keytech/Main.java
@@ -14,30 +14,33 @@ public class Main {
         JLabel label = new JLabel();
         label.setText("Dah!");
         label.setIcon(image);
-        label.setVerticalAlignment(JLabel.BOTTOM);
-        label.setHorizontalAlignment(JLabel.RIGHT);
+        /**@the below line only apply when using border layouts
+         * */
+        //label.setVerticalAlignment(JLabel.BOTTOM);
+        //label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(100, 100, 75, 75);//if not using boarder layout
 
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0, 0, 250, 250);
-        redPanel.setLayout(new BorderLayout());
+        redPanel.setLayout(null);
 
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.blue);
         bluePanel.setBounds(250, 0, 250, 250);
-        bluePanel.setLayout(new BorderLayout());
+        bluePanel.setLayout(null);
 
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0, redPanel.getHeight(), 500, 250);
-        greenPanel.setLayout(new BorderLayout());
+        greenPanel.setLayout(null);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(750, 750);
         frame.setLayout(null);
         frame.setVisible(true);
-        bluePanel.add(label);
+        greenPanel.add(label);
         frame.add(redPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
