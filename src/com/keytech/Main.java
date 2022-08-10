7adb876c3b0fe2fc44a857de50002b7000bf5152
benNjogu <kamandemachafi@gmail.com>
2022-08-10 22:21:53 +0300

    JText field

--- src/com/keytech/Main.java
+++ src/com/keytech/Main.java
@@ -6,29 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //JOptionPane - pop up a standard dialog box that prompts users for a value or
-        //              informs them of something.
-
-        JOptionPane.showMessageDialog(null, "Bros b4 Hoes", "title", JOptionPane.PLAIN_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Bros b4 Hoes", "title", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Bros b4 Hoes", "title", JOptionPane.QUESTION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Bros b4 Hoes", "title", JOptionPane.WARNING_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Bros b4 Hoes", "title", JOptionPane.ERROR_MESSAGE);
-
-        JOptionPane.showConfirmDialog(null, "Bros b4 Hoes", "title", JOptionPane.WARNING_MESSAGE);
-
-        String name = JOptionPane.showInputDialog("what is your name?");
-        System.out.println(name);
-
-        String[] responses = {"No, u are awesome!", "Hi five", "*blush*"};
-        ImageIcon imageIcon = new ImageIcon(Main.class.getResource("/images/bro1.jpg"));
-        JOptionPane.showOptionDialog(null,
-                "you are awesome",
-                "secret message",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,
-                imageIcon,
-                responses,
-                0);
+        //JTextfield:- A GUI textbox component that can be used to add, set or get text.
     }
 }
