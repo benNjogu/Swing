21548a938832500b36575c19fd505bef9391b3a5
benNjogu <kamandemachafi@gmail.com>
2022-08-17 09:30:33 +0300

    MouseListener basics

--- src/com/keytech/MyMouseListenerFrame.java
+++ src/com/keytech/MyMouseListenerFrame.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseListener;
 public class MyMouseListenerFrame extends JFrame implements MouseListener {
 
     private JLabel label;
+    //icons
     private ImageIcon brocon;
     private ImageIcon brosforever;
     private ImageIcon point;
@@ -16,49 +17,51 @@ public class MyMouseListenerFrame extends JFrame implements MouseListener {
     MyMouseListenerFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
 
+        //init variables
         label = new JLabel();
-        label.setBounds(0, 0, 100, 100);
-        label.setBackground(Color.red);
-        label.setOpaque(true);
         this.addMouseListener(this);
 
+        brocon = new ImageIcon(MyMouseListenerFrame.class.getResource("/images/brocon.png"));
+        brosforever = new ImageIcon(MyMouseListenerFrame.class.getResource("/images/brosforever.png"));
+        suitup = new ImageIcon(MyMouseListenerFrame.class.getResource("/images/suitup.png"));
+        point = new ImageIcon(MyMouseListenerFrame.class.getResource("/images/point.png"));
+
+        label.setIcon(brocon);
+
         this.add(label);
+        this.pack();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         //Invoked when the mouse button has been clicked (pressed and released) on a component
-        System.out.println("You clicked the mouse");
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         //Invoked when a mouse button has been pressed on a component
-        System.out.println("You pressed the mouse");
-        label.setBackground(Color.yellow);
+        label.setIcon(suitup);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         //Invoked when a mouse button has been released on a component
-        System.out.println("You released the mouse");
-        label.setBackground(Color.green);
+        label.setIcon(brosforever);
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
         //Invoked when a mouse enters a component
-        System.out.println("You entered the mouse zone");
-        label.setBackground(Color.blue);
+        label.setIcon(point);
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         //Invoked when the mouse exits a component
-        System.out.println("You exited the mouse zone");
-        label.setBackground(Color.red);
+        label.setIcon(brocon);
     }
 }
