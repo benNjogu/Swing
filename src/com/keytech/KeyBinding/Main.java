59b0c709e73987737e5d7f38fc6dcbe2b74df025
benNjogu <kamandemachafi@gmail.com>
2022-08-17 11:49:11 +0300

    KeyBinding using arrow keys.

---
+++ src/com/keytech/KeyBinding/Main.java
@@ -0,0 +1,19 @@
+package com.keytech.KeyBinding;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * @Key_bindings =  bind an action to a keystroke
+         *                  don't require you to click a component to give it focus
+         *                  all swing components use key bindings
+         *                  increased flexibility compared to key listeners
+         *                  can assign key strokes to individual swing components
+         *                  more difficult to utilize and set up :(
+         * */
+
+        new Game();
+    }
+
+}
